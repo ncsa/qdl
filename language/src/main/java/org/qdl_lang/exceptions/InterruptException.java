01316426bad3773fc3b984d9d4330a9581bd3a61
jjg-123 <gaynor@illinois.edu>
2025-05-12 07:22:41 -0500

    https://github.com/ncsa/qdl/issues/120 notes added

--- language/src/main/java/org/qdl_lang/exceptions/InterruptException.java
+++ language/src/main/java/org/qdl_lang/exceptions/InterruptException.java
@@ -1,16 +1,39 @@
 package org.qdl_lang.exceptions;
 
 import org.qdl_lang.state.SIEntry;
+import org.qdl_lang.statements.Statement;
 
 /**
  * <p>Created by Jeff Gaynor<br>
  * on 10/25/20 at  2:35 PM
  */
-public class InterruptException extends QDLException {
-    public InterruptException(SIEntry siEntry) {
+public class InterruptException extends QDLExceptionWithTrace
+{
+    public InterruptException(Statement statement, SIEntry siEntry) {
+        super(statement);
         this.siEntry = siEntry;
     }
 
+    public InterruptException(Throwable cause, Statement statement, SIEntry siEntry) {
+        super(cause, statement);
+        this.siEntry = siEntry;
+    }
+
+    public InterruptException(String message, Statement statement, SIEntry siEntry) {
+        super(message, statement);
+        this.siEntry = siEntry;
+    }
+
+    public InterruptException(String message, Throwable cause, Statement statement, SIEntry siEntry) {
+        super(message, cause, statement);
+        this.siEntry = siEntry;
+    }
+
+    /*
+        public InterruptException(SIEntry siEntry) {
+            this.siEntry = siEntry;
+        }
+    */
     public SIEntry getSiEntry() {
         return siEntry;
     }
@@ -20,5 +43,6 @@ public class InterruptException extends QDLException {
     }
 
     SIEntry siEntry;
+
 
 }
