4d8e3d8e0a804447694253f7230d0ef83ae2c5d8
jjg-123 <gaynor@illinois.edu>
2025-10-27 12:12:39 -0500

    1.6.2.3.

--- language/src/main/java/org/qdl_lang/state/State.java
+++ language/src/main/java/org/qdl_lang/state/State.java
@@ -831,7 +831,7 @@ public class State extends FunctionState implements QDLConstants {
                                     <application-name>${project.name}</application-name>
                                     <application-title>OA4MP</application-title>
                                     <build-time>${timestamp}</build-time>
-                                    <implementation-version>1.6-QDL-SNAPSHOT</implementation-version>
+                                    <implementation-version>1.6.2.3</implementation-version>
                                     <implementation-build>${buildNumber}</implementation-build>
                                     <implementation-title>QDL</implementation-title>
                                 </manifestEntries>
